885d6646d32456414db1b54124cf5b7d8e8a0aa9
samnart1 <originalsamnart@gmail.com>
2025-07-14 13:19:25 +0200

    chore(logo): add opendatahub logo and eu logo

--- src/App.vue
+++ src/App.vue
@@ -5,173 +5,203 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
 <template>
-  <div id="app">
-    <div v-if="authenticated" class="topbar">
-      <img class="logo" alt="Vue logo" src="./assets/logo_bn.svg" />
-      <div class="routerHeader">
-        <h3>{{ siteName }}</h3>
-        <h4 v-if="currentRouteName">{{ currentRouteName }}</h4>
-      </div>
-      <b-nav class="navbar">
-        <b-nav-item
-          v-for="route in routes"
-          :to="route.link"
-          :key="route.link"
-          exact
-          exact-active-class="active"
-          >{{ route.name }}</b-nav-item
-        >
-        <div class="userBox">
-          Hi, {{ username }}
-          <b-nav-item v-if="username" v-on:click="logout()">Logout</b-nav-item>
-        </div>
-      </b-nav>
-    </div>
-    <div v-else>
-      <img class="bigLogo" alt="Vue logo" src="./assets/logo.png" />
-      <h3>{{ siteName }}</h3>
-    </div>
+    <div id="app">
+        <div v-if="authenticated" class="topbar">
 
-    <div class="content" v-if="dataLoaded || !authenticated">
-      <b-card title="Card Title" no-body>
-        <b-card-body class="text-center">
-          <router-view />
-        </b-card-body>
-      </b-card>
+            <a href="https://noi.bz.it/">
+                <img
+                    class="logo"
+                    alt="Vue logo"
+                    src="./assets/logo_bn.svg"
+                />
+            </a>
+            <a href="https://opendatahub.com/">
+                <img
+                    class="logo-noi"
+                    alt="Vue logo"
+                    src="https://opendatahub.com/img/NOI_OPENDATAHUB_NEW_BK_nospace-01.svg"
+                />
+            </a>
+            <a href="https://europa.provincia.bz.it/it/informazione-e-visibilita-fesr">
+                <img
+                    class="logo-eu"
+                    alt="Vue logo"
+                    src="https://databrowser.impact.digital.noi.bz.it/EFRmod.png"
+                />
+            </a>
+            <div class="routerHeader">
+                <h3>{{ siteName }}</h3>
+                <h4 v-if="currentRouteName">{{ currentRouteName }}</h4>
+            </div>
+            <b-nav class="navbar">
+                <b-nav-item
+                    v-for="route in routes"
+                    :to="route.link"
+                    :key="route.link"
+                    exact
+                    exact-active-class="active"
+                    >{{ route.name }}</b-nav-item
+                >
+                <div class="userBox">
+                    Hi, {{ username }}
+                    <b-nav-item v-if="username" v-on:click="logout()"
+                        >Logout</b-nav-item
+                    >
+                </div>
+            </b-nav>
+        </div>
+        <div v-else>
+            <img class="bigLogo" alt="Vue logo" src="./assets/logo.png" />
+            <h3>{{ siteName }}</h3>
+        </div>
+
+        <div class="content" v-if="dataLoaded || !authenticated">
+            <b-card title="Card Title" no-body>
+                <b-card-body class="text-center">
+                    <router-view />
+                </b-card-body>
+            </b-card>
+        </div>
+        <b-spinner v-else></b-spinner>
     </div>
-    <b-spinner v-else></b-spinner>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "app",
-  data() {
-    return {
-      username: null,
-      routes: [
-        { name: "Displays", link: "/displays" },
-        { name: "Templates", link: "/templates" },
-      ],
-      siteName: "eInk Display management site",
-    };
-  },
-  computed: {
-    currentRouteName() {
-      if (this.routes.some((e) => e.name === this.$route.name)) {
-        return this.$route.name;
-      }
-      return "";
+    name: "app",
+    data() {
+        return {
+            username: null,
+            routes: [
+                { name: "Displays", link: "/displays" },
+                { name: "Templates", link: "/templates" },
+            ],
+            siteName: "eInk Display management site",
+        };
     },
-    authenticated() {
-      return this.$store.state.authenticated;
-    },
-    dataLoaded() {
-      return this.$store.state.dataLoaded;
-    },
-  },
-  mounted() {
-    if (this.authenticated) {
-      this.$keycloak.loadUserInfo().then((info) => {
-        this.username = info.name;
-      });
-    }
-
-    if (!localStorage.getItem("noPrivacyMessage")) {
-      this.showPrivacyToast();
-    }
-  },
-  methods: {
-    logout() {
-      const redirectPath = window.location.origin + "/";
-      this.$keycloak.logout({ redirectUri: redirectPath });
-    },
-    showPrivacyToast() {
-      const id = `privacy-toast`;
-      const $okButton = this.$createElement(
-        "b-button",
-        {
-          on: {
-            click: () => {
-              this.$bvToast.hide(id);
-              localStorage.setItem("noPrivacyMessage", true);
-            },
-          },
-          class: "toastCloseButton",
+    computed: {
+        currentRouteName() {
+            if (this.routes.some((e) => e.name === this.$route.name)) {
+                return this.$route.name;
+            }
+            return "";
         },
-        "OK, don't show again"
-      );
-
-      // Create the toast
-      this.$bvToast.toast(
-        [
-          "This site stores information regarding user identification. This information is necessary for the site to function.",
-          $okButton,
-        ],
-        {
-          id: id,
-          title: "",
-          toastClass: "privacyToast",
-          toaster: "b-toaster-bottom-center",
-          variant: "info",
-          noAutoHide: true,
-          noCloseButton: true,
-        }
-      );
+        authenticated() {
+            return this.$store.state.authenticated;
+        },
+        dataLoaded() {
+            return this.$store.state.dataLoaded;
+        },
     },
-  },
+    mounted() {
+        if (this.authenticated) {
+            this.$keycloak.loadUserInfo().then((info) => {
+                this.username = info.name;
+            });
+        }
+
+        if (!localStorage.getItem("noPrivacyMessage")) {
+            this.showPrivacyToast();
+        }
+    },
+    methods: {
+        logout() {
+            const redirectPath = window.location.origin + "/";
+            this.$keycloak.logout({ redirectUri: redirectPath });
+        },
+        showPrivacyToast() {
+            const id = `privacy-toast`;
+            const $okButton = this.$createElement(
+                "b-button",
+                {
+                    on: {
+                        click: () => {
+                            this.$bvToast.hide(id);
+                            localStorage.setItem("noPrivacyMessage", true);
+                        },
+                    },
+                    class: "toastCloseButton",
+                },
+                "OK, don't show again"
+            );
+
+            // Create the toast
+            this.$bvToast.toast(
+                [
+                    "This site stores information regarding user identification. This information is necessary for the site to function.",
+                    $okButton,
+                ],
+                {
+                    id: id,
+                    title: "",
+                    toastClass: "privacyToast",
+                    toaster: "b-toaster-bottom-center",
+                    variant: "info",
+                    noAutoHide: true,
+                    noCloseButton: true,
+                }
+            );
+        },
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 20px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 20px;
 }
 
 .content {
-  margin-top: 20px;
+    margin-top: 20px;
 }
 
 .topbar {
-  height: 75px;
+    height: 75px;
 }
 
 .navbar {
-  float: right;
+    float: right;
 }
 
 .logo {
-  margin-left: 20px;
-  margin-top: 8px;
-  float: left;
+    margin-left: 20px;
+    margin-top: 8px;
+    float: left;
 }
 
 .bigLogo {
-  margin-bottom: 25px;
+    margin-bottom: 25px;
 }
 
 .routerHeader {
-  float: left;
-  text-align: left;
-  margin-left: 30px;
+    float: left;
+    text-align: left;
+    margin-left: 30px;
 }
 
 .userBox {
-  border-radius: 8px;
-  border: 2px solid #aaaaaa;
+    border-radius: 8px;
+    border: 2px solid #aaaaaa;
 }
 
 .privacyToast {
-  padding-bottom: 35px;
+    padding-bottom: 35px;
 }
 
 .toastCloseButton {
-  float: right;
-  margin-top: 20px;
+    float: right;
+    margin-top: 20px;
+}
+
+.logo-eu,
+.logo-noi {
+  height: 40px;
+  margin: 15px;
+  /* display: block; */
 }
 </style>
